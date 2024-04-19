4ab79c86f9af52f9d436de02b25fb9d3d33bf4c2
aurigandrea <aurigandrea@gmail.com>
2024-04-19 14:58:14 +0100

    extra projetc added

--- script.js
+++ script.js
@@ -256,6 +256,8 @@ window.addEventListener('load', positionIcons);
                 <div id="carouselGovernment" class="carousel slide carousel-fade" data-ride="carousel">
 <ol class="carousel-indicators">
     <li data-target="carouselGovernment" data-slide-to="0" class="active"></li>
+        <li data-target="carouselFun" data-slide-to="1"></li>
+    <li data-target="carouselFun" data-slide-to="2"></li>
 </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
@@ -264,6 +266,12 @@ window.addEventListener('load', positionIcons);
                          <div class="carousel-item">
         <a href="ethics.html"><img src="../img/ethics.png" alt="ethics"></a>
                         </div>
+
+                        <div class="carousel-item">
+        <a href="https://storymaps.arcgis.com/stories/c775949b87264a55b00987a619825651"><img src="../img/religion.png" alt="religion"></a>
+                        </div>
+
+
 
                         <!-- Add more government images here -->
                     </div>
